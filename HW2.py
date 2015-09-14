b54f34e94446084e9246c3bca459daa2087fb197
TONO-SAMA <jdelapena@luc.edu>
2015-09-14 16:55:08 -0500

    Final Data Analysis
    There doesn’t seem to be any correlation.

--- HW2.py
+++ HW2.py
@@ -8,6 +8,7 @@ def landlordWards(): # gets wards from Problem Landlord CSV file
         landlordWards = []
         for row in wardReader:
             landlordWards.append(row[5]) #5th row has ward numbers
+        del landlordWards[0] #deletes the "WARD" row title
         return landlordWards
                 
 
@@ -17,6 +18,7 @@ def crimeWards(): # get wards from crimes in the past year
         crimeWards = []
         for row in wardReader:
             crimeWards.append(row[10]) #10th row has ward numbers
+        del crimeWards[0] #deletes the "WARD" row title
         return crimeWards
 
 def main():
@@ -29,6 +31,8 @@ def main():
 
     print("The wards with the most to least notable problem landlords are: ", landlordWardCount) #prints out list of wards and number of instances
     print("The wards with the most to least crime in the past year are: ", crimeWardCount) #prints out list of wards and number of instances
+    print("")
+    print("There seems to be little correlation between the number of crimes and problematic landlords.")
 
 main()
 
